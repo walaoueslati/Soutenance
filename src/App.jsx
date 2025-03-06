1da3ffd9e56707ee25d5310d3cd5cbe238af809c
unknown <bhksalma0@gmail.com>
2025-03-06 13:27:20 +0100

    Ajout de mon travail1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,7 +5,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>hello world salma</h1>
+        <h1>hello world salma 1</h1>
       </div>
      
     </>
